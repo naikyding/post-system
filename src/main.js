33acdb0d547df7056c3a393d71fcc0f15ae4038a
NaikyDing <74ding@gmail.com>
2023-05-10 11:36:20 +0800

    add: vuetify && icon

--- src/main.js
+++ src/main.js
@@ -6,15 +6,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// Vuetify icons
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+  },
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
-
-
-
-
-
-
-
+app.use(router).use(vuetify)
 app.mount('#app')
