e5b0e66492bf034edd4e75e9e78fbd250b3a042c
NaikyDing <74ding@gmail.com>
2023-05-10 16:01:40 +0800

    feat: vuetify 抽離

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,24 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-// Vuetify icons
-import '@mdi/font/css/materialdesignicons.css'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi', // This is already the default value - only for display purposes
-  },
-})
+import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router).use(vuetify)
+app.use(router)
+app.use(vuetify)
 app.mount('#app')
+
+export default app
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,29 @@
+// Vuetify icons
+import '@mdi/font/css/materialdesignicons.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  // ICON
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+  },
+
+  // THEME
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      // custom themes https://vuetifyjs.com/en/features/theme/#changing-theme
+      dark: {},
+      light: {},
+    },
+  },
+})
+
+export default vuetify
