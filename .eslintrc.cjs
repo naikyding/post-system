84beee62155ca0b64191fec52def162f43e8cb35
NaikyDing <74ding@gmail.com>
2023-05-10 11:36:38 +0800

    feat: format rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,9 @@ module.exports = {
   root: true,
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': 'off',
+  },
 }
