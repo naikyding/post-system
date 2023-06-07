04f39d35c6c64c8d02b640a056c082c1dad21ab6
Naiky <74ding@gmail.com>
2023-06-07 11:28:19 +0800

    feat: catch log style

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -2,7 +2,11 @@ const catchAsync = (func) => async (data) => {
   try {
     await func(data)
   } catch (errors) {
-    console.warn('catchAsync Errors:', errors)
+    console.warn(
+      '%c🔥 catchAsync 發生錯誤:',
+      'background: #F2B33D; border-radius: 4px; color: #fff; padding: .3rem 1rem;',
+      errors,
+    )
   }
 }
 
