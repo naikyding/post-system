e13216ef18dcc4c5cac2a92e8de9f5e004ce312e
Naiky <74ding@gmail.com>
2023-06-07 17:57:27 +0800

    feat(request): API 錯誤顯示
    -

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -2,6 +2,14 @@ const catchAsync = (func) => async (data) => {
   try {
     await func(data)
   } catch (errors) {
+    if (errors.name === 'AxiosError') {
+      return console.warn(
+        '%c🔥 API 發生錯誤:',
+        'background: #F2B33D; border-radius: 4px; color: #fff; padding: .3rem 1rem;',
+        `${errors.config.baseURL}/${errors.config.url}`,
+        errors,
+      )
+    }
     console.warn(
       '%c🔥 catchAsync 發生錯誤:',
       'background: #F2B33D; border-radius: 4px; color: #fff; padding: .3rem 1rem;',
