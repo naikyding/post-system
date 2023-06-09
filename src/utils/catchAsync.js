aa6f3635fd2466f21a0e25a5b82b50cedb652b18
Naiky <74ding@gmail.com>
2023-06-10 07:42:55 +0800

    feat: errors catch 調整
    -

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,13 +1,48 @@
+import Swal from 'sweetalert2'
+
 const catchAsync = (func) => async (data) => {
   try {
     return await func(data)
   } catch (errors) {
-    if (errors.name === 'AxiosError') {
+    // 若後端有響應
+    if (errors.response) {
+      // format html 顯示字串
+      const formatText = errors.response.data.errors.reduce((init, cur, curIndex) => {
+        const str = `<li>❌ ${cur}</li>`
+        if (errors.response.data.errors.length === curIndex - 1) return (init += `${str}</ul>`)
+        return (init += str)
+      }, '<ul>')
+
+      // 前端 log
+      Swal.fire({
+        icon: 'error',
+        title: '🔥 請求失敗',
+        html: formatText,
+        width: '400px',
+      })
+
+      // log
       return console.warn(
-        '%c🔥 API 發生錯誤:',
+        '%c🔥 請求失敗:',
         'background: #F2B33D; border-radius: 4px; color: #fff; padding: .3rem 1rem;',
         `${errors.config.baseURL}/${errors.config.url}`,
-        errors,
+        errors.response.data.errors,
+      )
+    }
+    // 網路失敗
+    if (errors.message === 'Network Error') {
+      Swal.fire({
+        icon: 'error',
+        title: '🔥 網路連線失敗:',
+        text: errors.message,
+        width: '400px',
+      })
+
+      return console.warn(
+        '%c🔥 網路連線失敗:',
+        'background: #F2B33D; border-radius: 4px; color: #fff; padding: .3rem 1rem;',
+        `${errors.config.baseURL}/${errors.config.url}`,
+        errors.message,
       )
     }
     console.warn(
