433060c4bd1ddf897c22eb1167ed807ce4d9a093
Naiky <74ding@gmail.com>
2023-06-10 13:54:11 +0800

    feat: progress alert
    -

---
+++ src/components/dialog/progressAlert.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useAppStore } from '../../stores/app'
+
+const appStore = useAppStore()
+</script>
+
+<template>
+  <v-dialog v-model="appStore.progressStatus" width="100" persistent>
+    <v-card height="100" class="justify-center align-center rounded-xl">
+      <v-progress-circular :size="70" :width="7" color="teal" indeterminate></v-progress-circular>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style lang="scss" scoped></style>
