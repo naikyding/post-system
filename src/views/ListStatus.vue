f791332adf46bfad573c2c127c57f4958e4c47e2
Naiky <74ding@gmail.com>
2023-06-10 18:46:58 +0800

    feat: order list
    -

--- src/views/ListStatus.vue
+++ src/views/ListStatus.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import { useSystemOrderList } from '../stores/orders'
+const systemOrderStore = useSystemOrderList()
+
+systemOrderStore.getOrderList()
+</script>
 
 <template>
-  <h1>list status</h1>
+  <v-table fixed-header>
+    <thead>
+      <tr>
+        <th class="text-left">Name</th>
+        <th class="text-left">Calories</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in desserts" :key="item.name">
+        <td>{{ item.name }}</td>
+        <td>{{ item.calories }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+
+  {{ systemOrderStore.orderList }}
 </template>
 
 <style lang="scss" scoped></style>
