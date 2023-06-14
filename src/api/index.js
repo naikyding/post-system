ef5977abadb4dd6533fcb71272a534a2b77b9dc1
Naiky <74ding@gmail.com>
2023-06-14 17:36:08 +0800

    feat(api):  更新訂單項目 api
    -

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import { getProducts } from './products'
-import { createOrder, getOrderList, deleteOrder, deleteOrderItem } from './orders'
+import { createOrder, getOrderList, deleteOrder, updateOrder, deleteOrderItem } from './orders'
 
 export const getProductsAPI = getProducts
 
 export const getOrderListAPI = getOrderList
 export const createOrderAPI = createOrder
+export const updateOrderAPI = updateOrder
 export const deleteOrderAPI = deleteOrder
 export const deleteOrderItemAPI = deleteOrderItem
--- src/api/orders.js
+++ src/api/orders.js
@@ -2,5 +2,6 @@ import request from './request'
 
 export const getOrderList = () => request.get('/orders')
 export const createOrder = (list) => request.post('/orders', list)
+export const updateOrder = (id, updateData) => request.patch(`/orders/${id}`, updateData)
 export const deleteOrder = (id) => request.delete(`/orders/${id}`)
 export const deleteOrderItem = (id) => request.delete(`/orders/item/${id}`)
