da3cdeac0c59efbcb6edb76282d1e4ccb53ea8f5
NaikyDing <74ding@gmail.com>
2023-06-16 11:06:06 +0800

    feat: empty data component

---
+++ src/components/EmptyBox.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="text-center ma-14">
+    <v-icon icon="mdi-dropbox" size="5rem" />
+    <p class="text-caption">
+      <slot name="text">沒有資料</slot>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/statusList/components/DataTable.vue
+++ src/views/statusList/components/DataTable.vue
@@ -3,6 +3,7 @@ import { reactive } from 'vue'
 import { useSystemOrderList } from '@/stores/orders'
 import { dateFormat } from '@/utils/day'
 import Swal from 'sweetalert2'
+import EmptyBox from '@/components/EmptyBox.vue'
 
 const systemOrderStore = useSystemOrderList()
 systemOrderStore.getOrderList()
@@ -50,78 +51,85 @@ async function updateDialog(orderListID, updateData, callback) {
 </script>
 
 <template>
-  <v-table fixed-header height="">
-    <thead>
-      <tr class="text-caption">
-        <th class="text-left">執行狀態</th>
-        <th class="text-left">項目</th>
-        <th class="text-left">時間</th>
-        <th class="text-left">商品名稱</th>
-        <th class="text-left">加料</th>
-        <th class="text-left">數量</th>
-        <th class="text-left">付款狀態</th>
-        <th class="text-left">備註</th>
-        <th class="text-center">-</th>
-      </tr>
-    </thead>
-    <tbody>
-      <template v-for="items in systemOrderStore.orderList" :key="items._id">
-        <tr v-for="(product, index) in items.items" :key="product._id">
-          <td>
-            <v-switch
-              @change="
-                systemOrderStore.updateProductItemStatus(product.product._id, product.status)
-              "
-              hide-details
-              color="success"
-              v-model="product.status"
-            >
-              <template #label>
-                <span class="text-caption">
-                  {{ `${product.status ? '完成' : '未完成'}` }}
-                </span>
-              </template>
-            </v-switch>
-          </td>
-          <td class="text-caption">
-            {{ `${index + 1} / ${items.items.length}` }}
-          </td>
-          <td class="text-caption">
-            <div class="day">{{ dateFormat(items.createdAt).split(' ')[0] }}</div>
-            <div class="day">{{ dateFormat(items.createdAt).split(' ')[1] }}</div>
-          </td>
-          <td>
-            {{ product.product.name }}
-          </td>
-          <td class="text-caption">
-            <div v-for="extra in product.extras" :key="extra._id">
-              +<span class="bg-grey px-1 rounded-lg mx-1">{{ extra.type }}</span>
-              <span>{{ extra.name }}</span>
-            </div>
-          </td>
-          <td class="text-caption">{{ product.quantity }}</td>
-          <td>
-            <span
-              class="px-2 py-1 rounded-lg text-caption"
-              :class="[items.isPaid ? 'bg-success' : 'bg-error']"
-            >
-              {{ items.isPaid ? '已付款' : '未付款' }}</span
-            >
-          </td>
-          <td class="text-caption">{{ items.note }}</td>
-          <td class="text-center">
-            <v-btn
-              @click="showOrderListDetails(items)"
-              size="x-small"
-              icon="mdi-dots-vertical"
-              variant="text"
-            >
-            </v-btn>
-          </td>
+  <!-- 沒有資料 -->
+  <template v-if="systemOrderStore.orderList.length < 1">
+    <EmptyBox />
+  </template>
+
+  <template v-else>
+    <v-table fixed-header>
+      <thead>
+        <tr class="text-caption">
+          <th class="text-left">執行狀態</th>
+          <th class="text-left">項目</th>
+          <th class="text-left">時間</th>
+          <th class="text-left">商品名稱</th>
+          <th class="text-left">加料</th>
+          <th class="text-left">數量</th>
+          <th class="text-left">付款狀態</th>
+          <th class="text-left">備註</th>
+          <th class="text-center">-</th>
         </tr>
-      </template>
-    </tbody>
-  </v-table>
+      </thead>
+      <tbody>
+        <template v-for="items in systemOrderStore.orderList" :key="items._id">
+          <tr v-for="(product, index) in items.items" :key="product._id">
+            <td>
+              <v-switch
+                @change="
+                  systemOrderStore.updateProductItemStatus(product.product._id, product.status)
+                "
+                hide-details
+                color="success"
+                v-model="product.status"
+              >
+                <template #label>
+                  <span class="text-caption">
+                    {{ `${product.status ? '完成' : '未完成'}` }}
+                  </span>
+                </template>
+              </v-switch>
+            </td>
+            <td class="text-caption">
+              {{ `${index + 1} / ${items.items.length}` }}
+            </td>
+            <td class="text-caption">
+              <div class="day">{{ dateFormat(items.createdAt).split(' ')[0] }}</div>
+              <div class="day">{{ dateFormat(items.createdAt).split(' ')[1] }}</div>
+            </td>
+            <td>
+              {{ product.product.name }}
+            </td>
+            <td class="text-caption">
+              <div v-for="extra in product.extras" :key="extra._id">
+                +<span class="bg-grey px-1 rounded-lg mx-1">{{ extra.type }}</span>
+                <span>{{ extra.name }}</span>
+              </div>
+            </td>
+            <td class="text-caption">{{ product.quantity }}</td>
+            <td>
+              <span
+                class="px-2 py-1 rounded-lg text-caption"
+                :class="[items.isPaid ? 'bg-success' : 'bg-error']"
+              >
+                {{ items.isPaid ? '已付款' : '未付款' }}</span
+              >
+            </td>
+            <td class="text-caption">{{ items.note }}</td>
+            <td class="text-center">
+              <v-btn
+                @click="showOrderListDetails(items)"
+                size="x-small"
+                icon="mdi-dots-vertical"
+                variant="text"
+              >
+              </v-btn>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </v-table>
+  </template>
 
   <!-- confirm Dialog -->
   <v-dialog transition="dialog-bottom-transition" v-model="dialog.confirmOrderList" width="400">
