a5677985a8133cc32f0f1532626cd375df6b55b5
Naiky <74ding@gmail.com>
2023-06-20 17:33:04 +0800

    feat: login layout
    -

---
+++ src/views/user/Login.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const valid = ref(false)
+</script>
+
+<template>
+  <div class="h-screen d-flex justify-center align-center">
+    <v-form class="w-360px text-center px-4" v-model="valid">
+      <h2 class="mb-4">系統登入</h2>
+      <v-text-field
+        type="text"
+        prepend-inner-icon="mdi-account"
+        label="帳號"
+        variant="solo-filled"
+        clearable
+      ></v-text-field>
+      <v-text-field
+        type="password"
+        label="密碼"
+        prepend-inner-icon="mdi-lock"
+        variant="solo-filled"
+        clearable
+      ></v-text-field>
+      <v-btn color="primary" :disabled="valid" type="submit" size="x-large" block>登入</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.w-360px {
+  width: 360px;
+}
+</style>
