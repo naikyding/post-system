72f27af94c61a9b919aecb4bc82522582a5944e0
Naiky <74ding@gmail.com>
2023-06-26 11:56:13 +0800

    feat(login): base layout && validate
    -

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,25 +1,43 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, onUpdated, reactive, ref } from 'vue'
 
-const valid = ref(false)
+const loginForm = ref(null)
+const form = reactive({
+  // 表單狀態
+  valid: false,
+
+  account: '',
+  password: '',
+
+  // 驗証條件
+  rules: {
+    account: [(account) => !!account || '請輸入帳號'],
+    password: [(password) => (!password ? '請輸入密碼' : true)],
+  },
+})
 </script>
 
 <template>
   <div class="h-screen d-flex justify-center align-center">
-    <v-form class="w-360px text-center px-4" v-model="valid">
-      <h2 class="mb-4">系統登入</h2>
+    <v-form ref="loginForm" class="w-360px px-4" v-model="form.valid" @submit.prevent>
+      <h2 class="mb-4 text-center">系統登入</h2>
 
       <!-- 帳號 -->
       <v-text-field
+        v-model="form.account"
+        :rules="form.rules.account"
         type="text"
-        prepend-inner-icon="mdi-account"
         label="帳號"
+        prepend-inner-icon="mdi-account"
         variant="solo-filled"
         clearable
       ></v-text-field>
 
       <!-- 密碼 -->
       <v-text-field
+        v-model="form.password"
+        :rules="form.rules.password"
+        class="my-2"
         type="password"
         label="密碼"
         prepend-inner-icon="mdi-lock"
@@ -28,7 +46,7 @@ const valid = ref(false)
       ></v-text-field>
 
       <!-- 登入 -->
-      <v-btn color="primary" :disabled="valid" type="submit" size="x-large" block>登入</v-btn>
+      <v-btn color="primary" :disabled="!form.valid" type="submit" size="x-large" block>登入</v-btn>
     </v-form>
   </div>
 </template>
