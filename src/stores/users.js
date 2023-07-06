d5f59563b6b3ddfa42c0c5ad434a4e771248ce7c
Naiky <74ding@gmail.com>
2023-07-06 18:02:03 +0800

    feat(login): login 功能
    -

--- src/stores/users.js
+++ src/stores/users.js
@@ -1,12 +1,20 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { loginAPI } from '@/api'
 
 export const useUerStore = defineStore('user', () => {
   const customer = ref('6476f4088940f49853aa062e')
   const agent = ref('64741f07778d6a978ef85f10')
 
+  async function login(emailNPassword) {
+    const { data } = await loginAPI(emailNPassword)
+    return data
+  }
+
   return {
     customer,
     agent,
+
+    login,
   }
 })
--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,19 +1,31 @@
 <script setup>
 import { computed, onUpdated, reactive, ref } from 'vue'
+import { useUerStore } from '@/stores/users'
+import catchAsync from '@/utils/catchAsync'
+import { errorFunction } from '@/utils/catchAsync'
 
+const uerStore = useUerStore()
 const loginForm = ref(null)
 const form = reactive({
   // 表單狀態
   valid: false,
 
-  account: '',
+  email: '',
   password: '',
 
   // 驗証條件
   rules: {
-    account: [(account) => !!account || '請輸入帳號'],
+    email: [(email) => !!email || '請輸入 email'],
     password: [(password) => (!password ? '請輸入密碼' : true)],
   },
+})
+
+const login = catchAsync(async () => {
+  const res = await uerStore.login({
+    email: form.email,
+    password: form.password,
+  })
+  console.log(res)
 })
 </script>
 
@@ -24,10 +36,10 @@ const form = reactive({
 
       <!-- 帳號 -->
       <v-text-field
-        v-model="form.account"
-        :rules="form.rules.account"
+        v-model="form.email"
+        :rules="form.rules.email"
         type="text"
-        label="帳號"
+        label="電子信箱"
         prepend-inner-icon="mdi-account"
         variant="solo-filled"
         clearable
@@ -46,7 +58,9 @@ const form = reactive({
       ></v-text-field>
 
       <!-- 登入 -->
-      <v-btn color="primary" :disabled="!form.valid" type="submit" size="x-large" block>登入</v-btn>
+      <v-btn color="primary" size="x-large" block :disabled="!form.valid" @click="login"
+        >登入</v-btn
+      >
     </v-form>
   </div>
 </template>
