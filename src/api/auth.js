b800103fb71a08616bcb49804eb9c9c049174447
Naiky <74ding@gmail.com>
2023-07-10 17:20:18 +0800

    feat(api): 取得使用者基本資料
    -

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,3 +2,4 @@ import request from './request'
 
 export const login = (emailNPassword) => request.post('/auth/login', emailNPassword)
 export const refreshToken = (refreshToken) => request.post('/auth/refresh-token', refreshToken)
+export const gerUserBaseInfo = () => request.get('/users/base-info')
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import { getProducts } from './products'
 import { createOrder, getOrderList, deleteOrder, updateOrder, deleteOrderItem } from './orders'
-import { login, refreshToken } from './auth'
+import { login, refreshToken, gerUserBaseInfo } from './auth'
 
 export const getProductsAPI = getProducts
 
@@ -12,3 +12,4 @@ export const deleteOrderItemAPI = deleteOrderItem
 
 export const loginAPI = login
 export const refreshTokenAPI = refreshToken
+export const gerUserBaseInfoAPI = gerUserBaseInfo
