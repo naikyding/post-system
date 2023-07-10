8763ae9da6d502de75929ac55dccea466c59d55f
Naiky <74ding@gmail.com>
2023-07-10 11:25:12 +0800

    feat(router): login page beforeEach
    - 若已登入，不再進入 login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
+import { useUserStore } from '../stores/users'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+
+  // 如果已經登入，就導首頁
+  if (to.name === 'Login' && userStore.isLogin) {
+    if (to.query.redirect) {
+      return next({ path: to.query.redirect })
+    }
+    return next({ path: '/' })
+  }
+
+  next()
 })
 
 export default router
