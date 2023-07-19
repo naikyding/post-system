216b08287ed64ef5042096fa6b025976739edb15
Naiky <74ding@gmail.com>
2023-07-19 10:24:59 +0800

    feat: dashboard base layout
    -

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,30 @@
-<script setup></script>
+<script setup>
+import { useSystemOrderList } from '../stores/orders'
+
+const SystemOrderListStore = useSystemOrderList()
+</script>
 
 <template>
-  <h1>HOME</h1>
+  <div class="h-screen pa-4">
+    <div class="d-sm-flex justify-space-between">
+      <h3>Dashboard</h3>
+      <div>
+        <v-btn
+          v-for="(date, index) in SystemOrderListStore.selectDate"
+          :key="date + index"
+          density="compact"
+        >
+          {{ date.name }}
+        </v-btn>
+      </div>
+    </div>
+    <v-container>
+      <v-row>
+        <v-col cols="6" sm="3">營業額</v-col>
+        <v-col cols="6" sm="3">來客數</v-col>
+        <v-col cols="6" sm="3">客單價</v-col>
+        <v-col cols="6" sm="3">--</v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
