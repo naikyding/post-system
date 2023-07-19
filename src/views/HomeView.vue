c9c1280bd70e06ef664660ca553548f436b2ede7
Naiky <74ding@gmail.com>
2023-07-19 17:58:56 +0800

    feat: 切換取得資料日期
    -

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
 <script setup>
 import { useSystemOrderList } from '../stores/orders'
+import AverageOrderValue from './dashboard/AverageOrderValue.vue'
+import Revenue from './dashboard/Revenue.vue'
+import Visitors from './dashboard/Visitors.vue'
+import dayJs from 'dayjs'
+import quarterOfYear from 'dayjs/plugin/quarterOfYear'
+dayJs.extend(quarterOfYear)
+import { dateFormat } from '../utils/day'
 
 const SystemOrderListStore = useSystemOrderList()
+
+function changeSearchData(type) {
+  if (SystemOrderListStore.activeRange === type) return false
+  SystemOrderListStore.activeRange = type
+  SystemOrderListStore.activeListDate.from = dateFormat(
+    dayJs().subtract(type === 'day' ? 0 : 1, type),
+  )
+
+  SystemOrderListStore.getOrderListFromSystem(SystemOrderListStore.activeListDate)
+}
 </script>
 
 <template>
@@ -13,6 +30,8 @@ const SystemOrderListStore = useSystemOrderList()
           v-for="(date, index) in SystemOrderListStore.selectDate"
           :key="date + index"
           density="compact"
+          :active="date.range === SystemOrderListStore.activeRange"
+          @click="changeSearchData(date.range)"
         >
           {{ date.name }}
         </v-btn>
@@ -20,9 +39,18 @@ const SystemOrderListStore = useSystemOrderList()
     </div>
     <v-container>
       <v-row>
-        <v-col cols="6" sm="3">營業額</v-col>
-        <v-col cols="6" sm="3">來客數</v-col>
-        <v-col cols="6" sm="3">客單價</v-col>
+        <v-col cols="6" sm="3">
+          <!-- 營業額 -->
+          <Revenue />
+        </v-col>
+        <v-col cols="6" sm="3">
+          <!-- 來客數 -->
+          <Visitors />
+        </v-col>
+        <v-col cols="6" sm="3">
+          <!-- 客單價 -->
+          <AverageOrderValue />
+        </v-col>
         <v-col cols="6" sm="3">--</v-col>
       </v-row>
     </v-container>
