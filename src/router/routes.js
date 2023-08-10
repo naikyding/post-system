fcd2e3bc473bc6907271303ae4e9f0b68a9cf339
NaikyDing <74ding@gmail.com>
2023-08-10 10:42:27 +0800

    fix: dynamic routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,12 @@
-import Login from '../views/user/Login.vue'
-import Layout from '../views/layout/index.vue'
-
 const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: Login,
+    component: () => import('@/views/user/Login.vue'),
   },
   {
     path: '/',
-    component: Layout,
+    component: () => import('@/views/layout/index.vue'),
     redirect: '/dashboard',
     children: [
       {
@@ -32,16 +29,7 @@ const routes = [
 
   {
     path: '/dash',
-    component: Layout,
-  },
-
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue'),
+    component: () => import('@/views/user/Login.vue'),
   },
 ]
 
