540d0adb600bbbe8dbd68b55142c2a78d04c70d8
NaikyDing <74ding@gmail.com>
2023-08-16 17:52:37 +0800

    feat: chart js

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,8 @@ import AverageOrderValue from './dashboard/AverageOrderValue.vue'
 import Revenue from './dashboard/Revenue.vue'
 import Visitors from './dashboard/Visitors.vue'
 import Quantity from './dashboard/Quantity.vue'
+import TopProduct from './dashboard/TopProduct.vue'
+
 import dayJs from 'dayjs'
 
 import quarterOfYear from 'dayjs/plugin/quarterOfYear'
@@ -105,6 +107,11 @@ function changeSearchData(type) {
         </v-col>
         <v-col cols="12" sm="6" md="3" class="pa-1">
           <Quantity />
+        </v-col>
+
+        <!-- Chart -->
+        <v-col cols="12" class="px-0">
+          <TopProduct class="bg-red" />
         </v-col>
       </v-row>
     </v-container>
