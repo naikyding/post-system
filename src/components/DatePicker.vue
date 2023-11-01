9e4982203888c8c1f8052d3837cc0f0a1e1209f2
Naiky <74ding@gmail.com>
2023-11-01 16:03:41 +0800

    feat(訂單狀態): 日期選擇器 設置
    -

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,32 +1,63 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import dayjs from 'dayjs'
+import { dateFormat } from '@/utils/day'
+
+const props = defineProps(['activeDate'])
+const emit = defineEmits(['searchList'])
 
 const bottomSheet = ref(false)
+const datePicker = ref(null)
+const today = dateFormat(dayjs())
 
 const datePickerConfig = ref({
   // 組件內容設置
   attributes: [
     // 標記今天
     {
-      key: 'today',
-      highlight: {
-        fillMode: 'outline',
+      content: {
+        style: {
+          color: 'lightgreen',
+          fontStyle: 'italic',
+        },
       },
-      dates: new Date(),
+      dates: today,
     },
   ],
 
   // 日期選擇
-  range: {
-    start: null,
-    end: null,
-  },
+  date: today,
 
   // 日期選擇格式
   masks: {
     modelValue: 'YYYY-MM-DD',
   },
+
+  // 禁用日期
+  disabledDates: [{ start: dateFormat(dayjs().add(1, 'day')), end: null }],
 })
+
+function buttonContentDisplay(today, currentDate) {
+  if (currentDate === today) return 'Today'
+  else return currentDate
+}
+
+const buttonDisplayContent = computed(() => {
+  if (datePickerConfig.value.date === today) return 'Today'
+  else return datePickerConfig.value.date
+})
+
+// 日期設置今日
+function selectToday(datePickerConfig) {
+  datePickerConfig.date = today
+  datePicker.value.move(today)
+}
+
+// 依設置日期取得資料
+function searchData(searchDate) {
+  emit('searchList', searchDate)
+  bottomSheet.value = false
+}
 </script>
 
 <template>
@@ -39,31 +70,46 @@ const datePickerConfig = ref({
     <template v-slot:prepend>
       <v-icon icon="mdi-tune-variant"></v-icon>
     </template>
-    <span> 2023-10-31 ~ 2023-10-31 </span>
+    <span>{{ buttonContentDisplay(today, props.activeDate) }}</span>
   </v-btn>
 
   <v-bottom-sheet v-model="bottomSheet">
-    {{ datePickerConfig.range }}
-    <v-card class="rounded-t-lg">
+    <v-card class="rounded-t-xl py-4">
+      <div class="text-center font-italic text-grey">{{ buttonDisplayContent }}</div>
       <VDatePicker
-        v-model.string="datePickerConfig.range"
-        is-range
-        borderless
-        transparent
-        title-position="left"
-        expanded
-        is-dark
+        v-model.string="datePickerConfig.date"
         :attributes="datePickerConfig.attributes"
         :masks="datePickerConfig.masks"
+        :disabled-dates="datePickerConfig.disabledDates"
+        ref="datePicker"
+        mode="date"
+        borderless
+        transparent
+        expanded
+        is-dark
       >
         <template #footer>
           <div class="px-4">
-            <v-btn block class="text-none mb-4" color="success" size="x-large" variant="flat">
-              OK
+            <v-btn
+              @click="selectToday(datePickerConfig)"
+              block
+              class="text-none mb-4"
+              color="warning"
+              size="x-large"
+              variant="flat"
+            >
+              Today
             </v-btn>
 
-            <v-btn block class="text-none mb-4" color="error" size="x-large" variant="flat">
-              CANCEL
+            <v-btn
+              @click="searchData(datePickerConfig.date)"
+              block
+              class="text-none mb-4"
+              color="success"
+              size="x-large"
+              variant="flat"
+            >
+              Search
             </v-btn>
           </div>
         </template>
