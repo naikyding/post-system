e7146b5d29ffde82958e3be40ac2fb7e500e3733
Naiky <74ding@gmail.com>
2023-11-02 18:54:18 +0800

    feat: 調整為共用，動態 range
    -

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -6,10 +6,17 @@ import { dateFormat } from '@/utils/day'
 const props = defineProps(['activeDate', 'isRange'])
 const emit = defineEmits(['searchList'])
 
+// 動態修飾符
+const modifiers = computed(() => ({
+  range: props.isRange || false,
+  string: true,
+}))
+
+console.log(props)
+
 const bottomSheet = ref(false)
 const datePicker = ref(null)
 const today = dateFormat(dayjs())
-
 const datePickerConfig = ref({
   // 組件內容設置
   attributes: [
@@ -26,7 +33,7 @@ const datePickerConfig = ref({
   ],
 
   // 日期選擇
-  date: today,
+  date: props.isRange ? { start: today, end: today } : today,
 
   // 日期選擇格式
   masks: {
@@ -38,18 +45,35 @@ const datePickerConfig = ref({
 })
 
 function buttonContentDisplay(today, currentDate) {
+  if (props.isRange) {
+    const { from, to } = currentDate
+    if (from === to && from === today) return 'Today'
+    return `${from} ~ ${to}`
+  }
   if (currentDate === today) return 'Today'
   else return currentDate
 }
 
 const buttonDisplayContent = computed(() => {
+  if (props.isRange) {
+    const { start, end } = datePickerConfig.value.date
+    if (datePickerConfig.value.date === today || (start === end && start === today)) return 'Today'
+    return `${start} ~ ${end}`
+  }
+
   if (datePickerConfig.value.date === today) return 'Today'
   else return datePickerConfig.value.date
 })
 
 // 日期設置今日
 function selectToday(datePickerConfig) {
-  datePickerConfig.date = today
+  if (props.isRange) {
+    datePickerConfig.date = {
+      start: today,
+      end: today,
+    }
+  } else datePickerConfig.date = today
+
   datePicker.value.move(today)
 }
 
@@ -75,14 +99,13 @@ function searchData(searchDate) {
 
   <v-bottom-sheet v-model="bottomSheet">
     <v-card class="rounded-t-xl py-4">
-      {{ props.isRange }}
       <div class="text-center font-italic text-grey">{{ buttonDisplayContent }}</div>
       <VDatePicker
-        v-model.string="datePickerConfig.date"
+        v-model="datePickerConfig.date"
+        :model-modifiers="modifiers"
         :attributes="datePickerConfig.attributes"
         :masks="datePickerConfig.masks"
         :disabled-dates="datePickerConfig.disabledDates"
-        :is-range="props.isRange"
         ref="datePicker"
         mode="date"
         borderless
