743ef91718d2b2eb3ea6c6b9aa9d4b1c90d782ac
Naiky <74ding@gmail.com>
2025-10-08 14:54:57 +0800

    fix: menu API

--- src/api/menus.js
+++ src/api/menus.js
@@ -2,6 +2,6 @@ import request from './request'
 
 export const getMenus = () => request.get('/menus')
 export const getMenusAndOperations = () => request.get('/menus?include=operations')
-export const createMenu = (data) => request.post('/menus', data)
-export const deleteMenu = (id) => request.post(`/menus/${id}`)
-export const updateMenu = ({ id, data }) => request.post(`/menus/${id}`, data)
+export const createMenu = (payload) => request.post('/menus', payload)
+export const deleteMenu = (id) => request.delete(`/menus/${id}`)
+export const updateMenu = (id, payload) => request.patch(`/menus/${id}`, payload)
