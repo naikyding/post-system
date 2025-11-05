401e9e69b77b3140355c2a107df14f62d3014b56
Naiky <74ding@gmail.com>
2025-11-05 11:55:51 +0800

    feat: form dialog component
    -

---
+++ src/components/dialog/FormDialog.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {} from 'vue'
+
+const status = defineModel()
+const emit = defineEmits(['submitEvent', 'cancelEvent', 'resetEvent'])
+const props = defineProps({
+  title: {
+    type: String,
+    default: '--',
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <v-dialog v-model="status" max-width="600">
+    <v-card :title="props.title" :prepend-icon="props.icon">
+      <v-form ref="form">
+        <v-card-text>
+          <slot />
+        </v-card-text>
+        <v-divider></v-divider>
+
+        <div class="pa-6">
+          <v-btn @click="emit('submitEvent')" class="" color="success" block> 保存 </v-btn>
+          <v-btn @click="emit('resetEvent')" class="mt-4" color="warning" block> 重置 </v-btn>
+          <v-btn @click="emit('cancelEvent')" class="mt-4" color="error" variant="outlined" block>
+            取消
+          </v-btn>
+        </div>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
