29f4891a4f2264e35f58b67c52b51a5825b54d9b
Naiky <74ding@gmail.com>
2026-06-04 19:25:36 +0800

    feat: isSuperAdmin
    -

--- src/views/global-setting/user/components/useTable.js
+++ src/views/global-setting/user/components/useTable.js
@@ -3,10 +3,11 @@ import { useUserStore } from '@/stores/users'
 export function useTable() {
   const userStore = useUserStore()
   const headers = [
+    { title: '#', key: 'index' },
     { title: '綽號', key: 'nickname' },
     { title: 'email', key: 'email' },
-    { title: '商家 / 角色', key: 'agentRoles' },
-    { title: '創建時間', key: 'createdAt' },
+    { title: '商家', key: 'agentRoles' },
+    { title: '角色', key: 'agentRoles' },
     { title: '操作', key: 'actions', align: 'center' },
   ]
 
