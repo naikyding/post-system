210884fcad3be071bdff427ac0a18f3b8342f225
Naiky <74ding@gmail.com>
2026-07-30 16:32:48 +0800

    fix: dashboard data for dynamic paymentType
    -

--- src/stores/orders.js
+++ src/stores/orders.js
@@ -460,7 +460,7 @@ export const useSystemOrderList = defineStore('systemOrder', () => {
     if (activeListDate.from && activeListDate.to)
       urlQueryString += `&from=${activeListDate.from}&to=${activeListDate.to}`
 
-    if (activeTab === 'false') return (urlQueryString += '&paid=false')
+    if (activeTab === 'false') return (urlQueryString += '&isPaid=false')
     if (activeTab === 'all') return urlQueryString
 
     return (urlQueryString += `&status=${activeTab}`)
